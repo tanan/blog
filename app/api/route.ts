199f91b75ff1e2d09cd0538d85303bae6ae29e7d
tanan <toshifumi.anan@gmail.com>
2024-03-09 22:31:42 +0900

    force-dynamic

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,4 +1,4 @@
-// export const dynamic = "force-dynamic"; // defaults to auto
+export const dynamic = "force-dynamic"; // defaults to auto
 export async function GET(request: Request) {
   const res = {
     message: "ok",
