69da35f9ca66d8da6aae32dd0a4f80fe03e7dd4e
tanan <toshifumi.anan@gmail.com>
2024-03-10 17:22:49 +0900

    remove check

--- lib/cms/client.tsx
+++ lib/cms/client.tsx
@@ -14,18 +14,18 @@ export type Blog = {
   category?: string;
 } & MicroCMSDate;
 
-if (!process.env.MICROCMS_SERVICE_DOMAIN) {
-  throw new Error("MICROCMS_SERVICE_DOMAIN is required");
-}
+// if (!process.env.MICROCMS_SERVICE_DOMAIN) {
+//   throw new Error("MICROCMS_SERVICE_DOMAIN is required");
+// }
 
-if (!process.env.MICROCMS_API_KEY) {
-  throw new Error("MICROCMS_API_KEY is required");
-}
+// if (!process.env.MICROCMS_API_KEY) {
+//   throw new Error("MICROCMS_API_KEY is required");
+// }
 
 // API取得用のクライアントを作成
 export const client = createClient({
-  serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
-  apiKey: process.env.MICROCMS_API_KEY,
+  serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN || "",
+  apiKey: process.env.MICROCMS_API_KEY || "",
 });
 
 // ブログ一覧を取得
