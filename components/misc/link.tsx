863a66251c792083e443e821fb2563d20c81383f
tanan <toshifumi.anan@gmail.com>
2024-04-06 08:19:55 +0900

    fix design

--- components/misc/link.tsx
+++ components/misc/link.tsx
@@ -5,13 +5,12 @@ import { Link as Scroll } from "react-scroll";
 export const Link = ({ to, content }: { to: string; content: string }) => {
   return (
     <Scroll
-      // activeClass="active"
+      activeClass="active"
       spy={true}
       to={to}
       smooth={true}
-      offset={-70}
       duration={500}
-      className="leading-8 text-sm text-gray-500 hover:text-gray-800 hover:rounded-md"
+      className="leading-8 text-sm text-gray-500 hover:text-gray-800 hover:rounded-md cursor-pointer"
     >
       {content}
     </Scroll>
