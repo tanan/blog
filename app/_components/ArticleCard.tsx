6670008db301811eaf0eb53815bfbd8a45631c15
tanan <toshifumi.anan@gmail.com>
2024-05-15 16:35:14 +0900

    fix design

--- app/_components/ArticleCard.tsx
+++ app/_components/ArticleCard.tsx
@@ -3,17 +3,17 @@ import { toFormatDate } from "@/lib/utils";
 
 export const ArticleCard = ({ post }: any) => {
   return (
-    <div className="mx-2 h-64 mb-4 max-w-[236px] duration-300 hover:opacity-70 bg-white rounded-md">
+    <div className="mx-auto mb-4 h-[clamp(200px,9.667rem+12.59vw,336px)] max-w-[300px] duration-300 hover:opacity-70 bg-white rounded-md">
       <a href={`/article/${post.id}`} className="article">
         <Image
           src={post.eyecatch.url}
           alt="eyecatch"
-          width={236}
-          height={118}
-          className="thumbnail h-[120px] mb-2 rounded-t-md"
+          width={300}
+          height={168}
+          className="thumbnail h-[clamp(130px,50%,168px)] mb-2 rounded-t-md"
         />
         <div className="px-4">
-          <h2 className="title font-semibold text-lg line-clamp-3">
+          <h2 className="title font-semibold text-[clamp(12px,1.5vw,16px)] line-clamp-3">
             {post.title}
           </h2>
           <div className="flex justify-end mt-2 items-center text-gray-700 gap-4">
