872750ef42e17f7c7c9308bad60e552518116930
tanan <toshifumi.anan@gmail.com>
2024-05-15 16:45:45 +0900

    fix date

--- app/_components/ArticleCard.tsx
+++ app/_components/ArticleCard.tsx
@@ -12,13 +12,13 @@ export const ArticleCard = ({ post }: any) => {
           height={168}
           className="thumbnail h-[clamp(130px,50%,168px)] mb-2 rounded-t-md"
         />
-        <div className="px-4">
+        <div className="relative px-4 h-[clamp(70px,45%,168px)]">
           <h2 className="title font-semibold text-[clamp(12px,1.5vw,16px)] line-clamp-3">
             {post.title}
           </h2>
-          <div className="flex justify-end mt-2 items-center text-gray-700 gap-4">
-            <span className="text-sm">{toFormatDate(post.publishedAt)}</span>
-          </div>
+          <span className="absolute right-4 bottom-6 md:bottom-2 sm:text-sm text-[12px] text-gray-700">
+            {toFormatDate(post.publishedAt)}
+          </span>
         </div>
       </a>
     </div>
