4a4bd245b93173199cc70697c13b56807fc2feb4
tanan <toshifumi.anan@gmail.com>
2024-05-16 16:58:57 +0900

    fix design

--- app/_components/ArticleCard.tsx
+++ app/_components/ArticleCard.tsx
@@ -3,23 +3,23 @@ import { toFormatDate } from "@/lib/utils";
 
 const ArticleCard = ({ post }: any) => {
   return (
-    <div className="mx-auto mb-4 h-[clamp(200px,9.667rem+12.59vw,336px)] max-w-[300px] duration-300 hover:opacity-70 bg-white rounded-md">
-      <a href={`/article/${post.id}`} className="article">
+    <div className="mx-auto mb-4 h-[clamp(240px,9.667rem+12.59vw,336px)] max-w-[300px] duration-300 hover:opacity-70 bg-white rounded-md">
+      <a href={`/article/${post.id}`} className="article h-full">
         <Image
           src={post.eyecatch.url}
           alt="eyecatch"
           width={300}
           height={168}
-          className="thumbnail h-[clamp(130px,50%,168px)] mb-2 rounded-t-md"
+          className="thumbnail h-[clamp(120px,50%,168px)] rounded-t-md"
         />
-        <div className="relative px-4 h-[clamp(70px,45%,168px)]">
+        <div className="relative mt-1 md:mt-2 px-4 h-[clamp(120px,50%,168px)]">
           <span className="bg-gray-200 px-2 py-1 rounded-md sm:text-sm text-[12px] text-gray-700">
             {post.category?.name}
           </span>
-          <h2 className="title mt-2 mx-1 font-semibold text-[clamp(12px,1.5vw,16px)] line-clamp-3">
+          <h2 className="title mt-1 md:mt-2 mx-1 font-semibold text-[clamp(14px,1.5vw,16px)] line-clamp-3">
             {post.title}
           </h2>
-          <span className="absolute right-4 bottom-6 md:bottom-2 sm:text-sm text-[12px] text-gray-700">
+          <span className="absolute right-4 bottom-3 md:bottom-2 sm:text-sm text-[12px] text-gray-700">
             {toFormatDate(post.publishedAt)}
           </span>
         </div>
