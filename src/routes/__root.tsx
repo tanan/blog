5a5331d3fb814487d8b21cb1e88d3b4027866847
tanan <toshifumi.anan@gmail.com>
2026-04-29 14:32:42 +0900

    feat: v2

---
+++ src/routes/__root.tsx
@@ -0,0 +1,31 @@
+import {
+	createRootRoute,
+	HeadContent,
+	Outlet,
+	Scripts,
+} from "@tanstack/react-router";
+
+export const Route = createRootRoute({
+	head: () => ({
+		meta: [
+			{ charSet: "utf-8" },
+			{ name: "viewport", content: "width=device-width, initial-scale=1" },
+			{ title: "blog" },
+		],
+	}),
+	shellComponent: RootDocument,
+});
+
+function RootDocument() {
+	return (
+		<html lang="en">
+			<head>
+				<HeadContent />
+			</head>
+			<body>
+				<Outlet />
+				<Scripts />
+			</body>
+		</html>
+	);
+}
